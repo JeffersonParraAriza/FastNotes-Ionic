3a19fd239a37ee2630b2c9b4fd5f269232522974
JeffersonParraAriza <jparra961023@gmail.com>
2025-11-11 22:35:44 -0500

    Versión inicial del proyecto HolaMundo en Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToast } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonToast],
 })
 export class HomePage {
   constructor() {}
+  mostrarToast = false;
+
+  mostrarMensaje() {
+    this.mostrarToast = true;
+  }
 }
